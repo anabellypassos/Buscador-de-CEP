c0d4bc3768af5b76857ae1aef0afef69cc169e6b
Anabelly <anabellybel123@gmail.com>
2023-12-02 22:46:12 -0300

    buccador de cep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import {FiSearch} from 'react-icons/fi';
+import '../src/Style.css';
+import api from './Services/api';
 
 function App() {
+
+const[input, setInput] = useState('');
+const[cep, setcep]= useState({});
+
+async function handleSearch (){
+  // 01310930/json/
+if(input=== ''){
+  alert("preencha algum cep!")
+  return;
+}
+
+try{
+const response = await api.get(`${input}/json`);
+setcep(response.data)
+setInput("")
+}catch{
+alert("Ops erro ao buscar");
+setInput("");
+}
+}
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="Container">
+      <h1 className="title">Buscador de CEP</h1>
+   
+
+    <div className="Container-input">
+      <input
+       type="text"
+       placeholder="Digite seu cep... "
+       value={input}
+       onChange={(e)=>setInput(e.target.value)}
+       />
+<button className="ButtonSearch" onClick={handleSearch}>
+<FiSearch size={25} color='#fff'/>
+</button>
+   </div> 
+
+{Object.keys(cep).length > 0 && (
+
+<main className='main'>
+<h2>CEP:{cep.cep}</h2>
+<span>{cep.logradouro}</span>
+<span>complemento:{cep.complemento}</span>
+<span>Bairro:{cep.bairro}</span>
+<span>{cep.localidade}-{cep.uf} </span>
+
+
+    </main>
+
+
+ 
+)  }
+
+
+
+  
+</div>
+
+
+
+
+
   );
 }
 
